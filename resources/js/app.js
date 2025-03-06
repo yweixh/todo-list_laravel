d9e01a18c654b2b8c7b7011ff8a2f8e1f7d0a85c
Hannah Elcano <helcano37@gmail.com>
2025-03-06 14:58:59 +0800

    Added plug-ins, AOS and Intro.JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import introJs from 'intro.js';
+import 'intro.js/minified/introjs.min.css';
+
+//AOS Plug-in
+AOS.init();
+
+
+//Intro.js Plug-in
+window.introJs = introJs;
--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    
 </div>
